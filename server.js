dfcb8dc12b482ab973dea398eab435b211b1280b
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-02-27 13:58:23 +0330

    update chrome location and attrs

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({
+    chromeLocation : '/usr/bin/chromium',
+    chromeFlags: [
+        '--no-sandbox',
+        '--headless',
+        '--disable-gpu',
+        '--remote-debugging-port=9222',
+        '--hide-scrollbars'
+    ]
+});
 
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.browserForceRestart());
